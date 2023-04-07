725b7e843893d6903aec174e516bfdcaa61756eb
Aditya Panda <panda.aditya93@gmail.com>
2023-04-07 11:33:45 +0530

    Wise-Route-React

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -13,58 +13,6 @@ function Form() {
                 <h1 className="text-3xl font-bold">Get in Touch</h1>
                 <h1 className="text-sm">We'd love to hear form you. Please fill out this form</h1>
             </div>
-            {/* <!-- form-part --> */}
-            {/* <form/>
-                <div className="md:px-10 mt-3 flex flex-col gap-3">
-                    <!-- first name and last name -->
-                    <div className="grid grid-cols-1 md:grid-cols-2">
-                        <!-- first name -->
-                        <div className="flex flex-col gap-1 ">
-                            <h1 className="text-lg font-medium aster">First Name</h1>
-                            <input className="text outline-none border border-1 border-slate-800 rounded-md p-2 placeholder:text-gray-400" type="text" placeholder="First Name" name="firstname" id="firstname" required autocomplete="off"/>
-                        </div>
-                        <!-- last name -->
-                        <div className="flex flex-col gap-1 md:ml-auto">
-                            <h1 className="text-lg font-medium aster">Last Name</h1>
-                            <input className="outline-none border border-1 border-slate-800 rounded-md p-2 placeholder:text-gray-400" type="text" placeholder="Last Name" name="lastname" id="lastname" required autocomplete="off"/>
-                        </div>
-                    </div>
-                    <!-- email -->
-                    <div className="flex flex-col gap-1">
-                        <h1 className="text-lg font-medium aster">Email</h1>
-                        <input className="outline-none border border-1 border-slate-800 rounded-md p-2 placeholder:text-gray-400" type="email" placeholder="you@company.com" name="email" id="email" required autocomplete="off"/>
-                    </div>
-                    <!-- phone no -->
-                    <div className="flex flex-col gap-1">
-                        <h1 className="text-lg font-medium aster">Phone number</h1>
-                        <div className="flex items-center gap-2">
-                            <!-- number according to country -->
-                            <div className="outline-none border border-1 border-slate-800 p-2 rounded-md">
-                                <select className="cursor-pointer" name="country-code" id="country-code">
-                                    <option value="91">+91</option>
-                                    <option value="92">+92</option>
-                                    <option value="44">+44</option>
-                                    <option value="95">+95</option>
-                                    <option value="94">+94</option>s
-                                </select>
-                            </div>
-                            <input className="outline-none border border-1 border-slate-800 rounded-md p-2 placeholder:text-gray-400 w-full" type="tel" placeholder="123-456-7890" name="phone number" id="phone-number" required autocomplete="off"/>
-                        </div>
-                    </div>
-                    <!-- message -->
-                    <div className="flex flex-col  gap-1">
-                        <h1 className="text-lg font-medium aster">Message</h1>
-                        <textarea className="outline-none border border-1 border-slate-800 rounded-md p-2" rows="6" cols="50" name="message" placeholder="Leave us a message..." type="message" id="message" required autocomplete="off"></textarea>
-                    </div>
-                    <!-- tick -->
-                    <div className="flex items-center gap-2">
-                        <input id="myradio" type="radio" name="" required="required" checked/>
-                        <h1><span className="text-gray-400">You agree to our friendly </span><span className="underline">Privacy Policy</span>.</h1>
-                    </div>
-                    <!-- submit -->
-                    <button className="bg-blue-500 text-white rounded-md px-4 py-2" type="submit">Submit</button>
-                </div>  
-            <form/> */}
             <form>
   <div class="mb-0 grid gap-6 p-4 md:grid-cols-2">
     {/* firstname */}
